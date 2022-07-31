6e794c1c6a517651c6d2333547bf666cc44f35be
rogerp4ssos <rogerp4ssos@gmail.com>
2022-07-31 15:36:50 -0300

    initial commit

---
+++ nada.cpp
@@ -0,0 +1,31 @@
+// g++ -std=c++17 -O2 -Wall a.cpp
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll;
+typedef tuple<int,int,int> tiii;
+
+#define all(v) v.begin(), v.end()
+#define INF INT_MAX
+#define MAX_N 112345
+#define MOD ((int)1e9+7)
+  
+void solve() {
+    int n;
+
+    cin >> n;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
