ef2479731b64a1271984d3c4358bf18f15390f5e
rogerp4ssos <rogerp4ssos@gmail.com>
2022-10-15 17:08:10 -0300

    Introductory Problems

---
+++ CSES Problem Set/trailing_zeros.cpp
@@ -0,0 +1,36 @@
+// g++ -std=c++17 -O2 -Wall trailing_zeros.cpp
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll;
+typedef tuple<int,int,int> tiii;
+
+#define sz(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define INF INT_MAX
+#define MAX_N 112345
+#define MOD ((int)1e9+7)
+  
+void solve() {
+    ll n;
+    cin >> n;
+
+    int ans = 0;
+    for (int i=5; n/i > 0; i*=5) 
+        ans += n/i;
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
